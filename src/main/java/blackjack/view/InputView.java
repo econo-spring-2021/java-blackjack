bb0875f469d812e7082b9d732dc1f91bf166fc33
Kwon770 <sckwon770@gmail.com>
2021-07-07 12:17:04 +0900

    feat: Impl method to parse name string

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -1,9 +1,15 @@
 package blackjack.view;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
 
-    public static List<String> parseNameString(String str) {
+    public static List<String> parseNameString(String str) throws IllegalArgumentException {
+        if (str.replaceAll(" ", "").equals(""))
+            throw new IllegalArgumentException();
+
+        String[] parsedNames = str.split(",");
+        return Arrays.asList(parsedNames);
     }
 }
--- src/test/java/InputTest.java
+++ src/test/java/InputTest.java
@@ -1,5 +1,6 @@
 import blackjack.view.InputView;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -8,6 +9,7 @@ import java.util.List;
 public class InputTest {
 
     @Test
+    @DisplayName("이름을 담긴 정상적인 문자열이 리스트로 변환되는지?")
     void test_parseNameString_withProperString() {
         List<String> names = InputView.parseNameString("a,b,c");
 
@@ -15,9 +17,10 @@ public class InputTest {
     }
 
     @Test
+    @DisplayName("이름 문자열이 공백일 경우, 예외를 던지는지?")
     void test_parseNameString_withEmpty() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            InputView.parseNameString(" ");
-        });
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+            InputView.parseNameString(" ")
+        );
     }
 }
