fcdc40ddc5ee92772bd1bcb126966967c4715ac3
Kwon770 <sckwon770@gmail.com>
2021-08-04 00:28:53 +0900

    refactor: Refactoring to remove unnecessary code in UserTest.java

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class UserTest {
+class UserTest {
 
     User user;
 
@@ -15,38 +15,38 @@ public class UserTest {
 
     @Test
     @DisplayName("카드가 5/5 인 경우, 유저의 카드 뽑는 판정이 옳은가")
-    public void test_isPossibleToGetMoreCard_with5and5Card() {
+    void test_isPossibleToGetMoreCard_with5and5Card() {
         user.addCard(new Card(CardShape.CLOVER, CardGrade.FIVE));
         user.addCard(new Card(CardShape.CLOVER, CardGrade.FIVE));
 
-        boolean rightJudgement = 5 + 5 < User.USER_LIMIT_CARD_VALUE;
+        final boolean rightJudgement = 5 + 5 < User.USER_LIMIT_CARD_VALUE;
         Assertions.assertEquals(rightJudgement, user.isPossibleToGetMoreCard());
     }
 
     @Test
     @DisplayName("카드가 10/10/10 인 경우, 유저의 카드 뽑는 판정이 옳은가")
-    public void test_isPossibleToGetMoreCard_with10and10and10Card() {
+    void test_isPossibleToGetMoreCard_with10and10and10Card() {
         user.addCard(new Card(CardShape.CLOVER, CardGrade.TEN));
         user.addCard(new Card(CardShape.CLOVER, CardGrade.TEN));
 
-        boolean rightJudgement = 10 + 10 < User.USER_LIMIT_CARD_VALUE;
+        final boolean rightJudgement = 10 + 10 < User.USER_LIMIT_CARD_VALUE;
         Assertions.assertEquals(rightJudgement, user.isPossibleToGetMoreCard());
     }
 
     @Test
     @DisplayName("카드가 10/10/ACE 인 경우, 유저의 카드 뽑는 판정이 옳은가")
-    public void test_isPossibleToGetMoreCard_with10and10andACECard() {
+    void test_isPossibleToGetMoreCard_with10and10andACECard() {
         user.addCard(new Card(CardShape.CLOVER, CardGrade.TEN));
         user.addCard(new Card(CardShape.CLOVER, CardGrade.TEN));
         user.addCard(new Card(CardShape.CLOVER, CardGrade.ACE));
 
-        boolean rightJudgement = 5 + 5 < User.USER_LIMIT_CARD_VALUE;
+        final boolean rightJudgement = 5 + 5 < User.USER_LIMIT_CARD_VALUE;
         Assertions.assertEquals(rightJudgement, user.isPossibleToGetMoreCard());
     }
 
     @Test
     @DisplayName("유저의 이긴 게임의 결과를 올바르게 판단하는가?")
-    public void test_judgeResult_IfWin() {
+    void test_judgeResult_IfWin() {
         user.addCard(new Card(CardShape.CLOVER, CardGrade.TEN));
         user.addCard(new Card(CardShape.CLOVER, CardGrade.TEN));
         user.addCard(new Card(CardShape.CLOVER, CardGrade.ACE));
@@ -57,7 +57,7 @@ public class UserTest {
 
     @Test
     @DisplayName("유저의 비긴 게임의 결과를 올바르게 판단하는가?")
-    public void test_judgeResult_IfDraw() {
+    void test_judgeResult_IfDraw() {
         user.addCard(new Card(CardShape.CLOVER, CardGrade.TEN));
         user.addCard(new Card(CardShape.CLOVER, CardGrade.TEN));
         user.addCard(new Card(CardShape.CLOVER, CardGrade.ACE));
